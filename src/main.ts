5857b62fd80d450c7cfb94599c7f88b87bfe8714
ake144 <tamiratgedi1@gmail.com>
2025-10-18 19:32:31 +0300

    course model and main  file is modified

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{
+    logger: ['error', 'warn', 'log'],
+  });
+
+  app.enableCors({
+     origin: '*',
+     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  // app.useLogger(app.get(Logger));
+
 
   const config = new DocumentBuilder()
     .setTitle('Course API')
