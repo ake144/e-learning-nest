e1587d3cb957ecd04593fe770ccdde95c8f49a90
ake1444 <aklilu.tamirat@partner.safaricom.et>
2025-11-06 11:33:36 +0300

    Update logger.middleware.ts
    the middleware res

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -5,7 +5,12 @@ import { Inject, Injectable, NestMiddleware } from "@nestjs/common";
 export class LoggerMiddleware implements NestMiddleware {
     use(req: any, res: any, next: () => void) {
         console.log(`Request... ${req.method} ${req.originalUrl}`);
+
+
+        console.log(`Response    ${res.statusCode}   {res.data}`)\
+        
         next();
     }
+    
 }
 
